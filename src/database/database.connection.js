ccd9087e02ede9e9759787b39b35764bffad7e2c
ana_btz <silva.anabtz@gmail.com>
2023-05-15 17:56:49 -0300

    config

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -9,4 +9,6 @@ const configDataBase = {
   connectionString: process.env.DATABASE_URL,
 };
 
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
 export const db = new Pool(configDataBase);
